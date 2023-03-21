bba7c31905a50793bdd2d915aced49456064034d
Luisa <luisafmv321@gmail.com>
2023-03-21 18:28:58 -0500

    Version 3

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,5 @@
 const {createApp} = Vue
+
 createApp({
     data(){
         return{
@@ -15,30 +16,86 @@ createApp({
     },
     methods:{
         procesar(){
-            alert(this.contacto.mascotas);
+            Swal.fire({
+                title: '¿Estás seguro(a) de enviar esta info?',
+                text: "Usaremos esta información para contactarnos con usted",
+                icon: 'info',
+                showCancelButton: true,
+                confirmButtonColor: '##0000ff',
+                cancelButtonColor: '#d33',
+                cancelButtonText: 'Cancelar',
+                confirmButtonText: 'Sí, envíala!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  Swal.fire(
+                    'Enviado!',
+                    'Pronto nos comunicaremos con usted.',
+                    'success'
+                  )
+                }
+              })
         },
         name(){
-            if(this.contacto.nombre === ''){
-                this.mostrarAlerta();
+            const namef = document.getElementById('name')
+            if(this.contacto.nombre.trim() === ''){
+                this.mostrarAlerta('El campo nombre es obligatorio', namef);
+                return;
             }
-            else{
-                console.log(this.contacto.nombre.trim());
-            }
+            this.limpiarAlerta(namef);
         },
-        lname(){console.log(this.contacto.apellido);},
-        tel(){console.log(this.contacto.telefono);},
-        message(){console.log(this.contacto.mensaje);},
-        pets(){console.log(this.contacto.mascotas);},
+        lname(){
+            const lnamef = document.getElementById('lname')
+            if(this.contacto.apellido.trim() === ''){
+                this.mostrarAlerta('El campo apellido es obligatorio', lnamef);
+                return;
+            }
+            this.limpiarAlerta(lnamef);
+        },
+        tel(){
+            const telf = document.getElementById('tel')
+            if(this.contacto.telefono.trim() === ''){
+                this.mostrarAlerta('El campo teléfono es obligatorio', telf);
+                return;
+            }
+            this.limpiarAlerta(telf);
+        },
+        message(){
+            const messagef = document.getElementById('message')
+            if(this.contacto.mensaje.trim() === ''){
+                this.mostrarAlerta('El campo mensaje es obligatorio', messagef);
+                return;
+            }
+            this.limpiarAlerta(messagef);
+        },
+        pets(){
+            const petsf = document.getElementById('pets')
+            if(this.contacto.mascotas.length === 0){
+                this.mostrarAlerta('Tienes que seleccionar mínimo una máscota', petsf);
+                return;
+            }
+            this.limpiarAlerta(petsf);
+        },
 
         //Error
-        mostrarAlerta(){
-            const form = document.getElementById('form')
-            const error = document.createElement('H5')
-            error.textContent = 'Hubo un error...';
-            error.classList.add('bg-light', 'p-1','text-center')
+        mostrarAlerta(mensaje,referencia){
+            const alerta = referencia.querySelector('.bg-light');
+            if(alerta){
+                alerta.remove();
+            }
 
-            form.appendChild(error)
+            const error = document.createElement('H5')
+            error.textContent = mensaje;
+            error.classList.add('bg-light','text-center','mt-2')
+
+            referencia.appendChild(error)
         },
+
+        limpiarAlerta(referencia){
+            const alerta = referencia.querySelector('.bg-light');
+            if(alerta){
+                alerta.remove();
+            }
+        }
     }
 })
 .mount('#app');
